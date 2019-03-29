2702beb616cfb405685fcc85a1e7841761d3df2a
Vladyslav Tkachenko <vladyslav.thedev@gmail.com>
2019-03-29 13:28:29 +0200

    Search Engine upd

--- src/App.js
+++ src/App.js
@@ -14,19 +14,21 @@ class  App extends Component {
     }
 
   }
-onSearchChange(event){
-  console.log(event.target.value);
-
+onSearchChange= (event)=> {
+  this.setState({ searchfield: event.target.value})
 }
 
 
 
   render() {
-  return(
+    const filteredRobots = this.state.robots.filter(robots =>{
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    })
+    return(
     <div className="tc">
       <h1>Robofriends</h1>
       <SearchBox searchChange={this.onSearchChange}/>
-      <CardList robots={this.state.robots}/>
+      <CardList robots={filteredRobots}/>
     </div>
   )
   }
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchBox= ({searchfield, searchChange})=> {
     return(
         <div className='pa2'>
-        <input type='search' 
+        <input type='search' className=" pa3 ba b--green bg-lightest blue"
         placeholder='Search Robots' 
         onChange={searchChange}
         />
