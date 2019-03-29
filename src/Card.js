0a52c9b2f483eef3f77c6e85c35c7c4d22679839
Vladyslav Tkachenko <vladyslav.thedev@gmail.com>
2019-03-29 12:05:46 +0200

    first react commit

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card =({name, email, id}) =>{
+    return(
+<div className= "tc bg-light-green dib br3 pa3 mr2 grow bw2 shadow-5">
+    <img alt="robots" src={`https://www.robohash.org/${id} 200x200}`} />
+    <h2>{name}</h2>
+<p>{email}</p>
+</div>
+
+    );
+}
+
+export default Card;
